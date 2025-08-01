b46e38664e4d394bbb74fed162b6e0f759b24250
codewithgracie <emmanuelgrace411@gmail.com>
2025-08-01 15:42:09 +0100

    colors update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-     "./index.html",
+    "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
+      colors: {
         success: "#10b981",
         info: "#3b82f6",
         error: "#ef4444",
@@ -18,7 +18,20 @@ export default {
         accent: {
           DEFAULT: "#5f5af6",
           focus: "#4d47f5"
-        }
+        },
+        navy: {
+          50: "#e7e9ef", 
+          100: "#c2c9d6",
+          200: "#a5f3fc",
+          300: "#697a9b",
+          400: "#5c6b8a",
+          450: "#465675",
+          500: "#384766",
+          600: "#0891b2",
+          700: "#26334d",
+          800: "#202b40",
+          900: "#192132",
+        },
       }
     },
   },
